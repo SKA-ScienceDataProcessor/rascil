2a0c5c014b67ee9dd0f3c85d54aa6aa82d57f903
timcornwell <realtimcornwell@gmail.com>
2020-02-04 12:55:53 +0000

    Another try at data installation check

--- rascil/processing_components/util/installation_checks.py
+++ rascil/processing_components/util/installation_checks.py
@@ -14,13 +14,17 @@ def check_data_directory(verbose=False):
     """ Check the data directory to see if it has been downloaded correctly
     """
     canary = rascil_path("data/configurations/LOWBD2.csv")
-    with open(canary, "r") as f:
-        first = f.read(1)
-        if first == "version https://git-lfs.github.com/spec/v1":
-            log.warning("The data directory is not available - perhaps git-lfs is needed")
-            print("The data directory is not available - perhaps git-lfs is needed")
-        else:
-            if verbose: print("The data directory appears to have been installed correctly")
+    try:
+        with open(canary, "r") as f:
+            first = f.read(1)
+            if first == "version https://git-lfs.github.com/spec/v1":
+                log.warning(
+                    "The data directory is not available - if required then git-lfs is needed")
+            else:
+                if verbose: print("The data directory appears to have been installed correctly")
+    except FileNotFoundError:
+        log.warning("The data directory is not available - if required then git-lfs is needed")
+
 
 if __name__ == "__main__":
     check_data_directory()
