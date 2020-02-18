0040862dc51620c806bc693d6e7232040046ddda
Danielle Fenech <danielle@openhpc-login-0.novalocal>
2020-02-18 16:26:20 +0000

    RFI bug fix

--- rascil/processing_components/simulation/rfi.py
+++ rascil/processing_components/simulation/rfi.py
@@ -140,7 +140,6 @@ def create_propagators_prop(config, frequency, nants_start, station_skip=1, atte
     """ Create a set of propagators
     :param config: configuration
     :param frequency: frequencies
-    :param trans_bw: float bandwidth of transmitter
     :param transmitter: str name of transmitter
     :param attenuation: generic attenuation value to use if no transmitter specified, else filename to load
     :param beamgainval: float generic beam gain value to use if no transmitter specified, else filename to load
@@ -371,8 +370,7 @@ def simulate_rfi_block_prop(bvis, nants_start, station_skip, attenuation_state=N
         # These are fixed in time but vary with frequency. The ad hoc attenuation
         # is set to produce signal roughly equal to noise at LOW
         propagators = create_propagators_prop(bvis.configuration, bvis.frequency, nants_start=nants_start,
-                                              trans_bw=emitter_bw, station_skip=station_skip,
-                                              attenuation=attenuation,
+                                              station_skip=station_skip, attenuation=attenuation,
                                               transmitter=trans, beamgainval=beamgain, trans_range=DTV_range)
         # Now calculate the RFI at the stations, based on the emitter and the propagators
         rfi_at_station = calculate_rfi_at_station(propagators, emitter)
