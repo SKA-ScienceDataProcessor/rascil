7a71853d379c9b6286b1884237a66da4b2639e22
Vlad Stolyarov <vlad.stolyarov@gmail.com>
2020-03-17 18:27:36 +0000

    First version of predict_wt

--- rascil/processing_components/imaging/wt.py
+++ rascil/processing_components/imaging/wt.py
@@ -36,8 +36,18 @@ try:
     	dirty = numpy.real(img)
     	return dirty
 
-    def predict_wt(bvis: Union[BlockVisibility, Visibility], model: Image, **kwargs) -> \
-            Union[BlockVisibility, Visibility]:
+    def dirty2vis(dirty, grid_size, theta, wtvis):
+    	# 1. Make uvgrid
+    	uvgrid = numpy.fft.ifft2(numpy.fft.fftshift(dirty))
+    	uvgrid = numpy.fft.fftshift(uvgrid)
+    	# 2. Make degridding
+    	flops = wtowers.degrid_simple_func(wtvis,uvgrid.reshape((grid_size*grid_size)),grid_size, theta)
+    	# 3. Return wtvis
+    	return wtvis
+
+
+    def predict_wt(bvis: BlockVisibility, model: Image, **kwargs) -> \
+            BlockVisibility:
         """ Predict using convolutional degridding.
         
         Wtowers version. https://gitlab.com/ska-telescope/py-wtowers
@@ -53,21 +63,44 @@ try:
         if model is None:
             return bvis
 
-        nthreads = get_parameter(kwargs, "threads", 4)
-        epsilon = get_parameter(kwargs, "epsilon", 1e-12)
-        do_wstacking = get_parameter(kwargs, "do_wstacking", True)
-        verbosity = get_parameter(kwargs, "verbosity", 2)
-        
         newbvis = copy_visibility(bvis, zero=True)
+        
+        # Create an empty vis_data structure
+        wtvis = wtowers.VIS_DATA()
+        
+        # Define the data to copy to wtvis
+        antenna_count = bvis.data['uvw'].shape[1]
+        bl_count = int(bvis.data.shape[0]*antenna_count*(antenna_count-1)/2) # bl_count = ntimes*nbases, each block will contain vis for one time count and one baseline
+        #nfreq = bvis.frequency.shape[0] # the same number of frequencies for each block
+        
+        # Allocate memory for wtvis structure, time_count = 1, freq_count = 1
+        status = wtowers.fill_vis_data_func(wtvis, antenna_count, bl_count, 1, 1)
+
+        ibl = 0
+        # Loop over ntime and and antennas in BlockVis, copy metadata
+        for itime in range(bvis.data.shape[0]):
+              for i1 in range(antenna_count):                
+                    for i2 in range(i1+1,antenna_count):
+                        wtvis.bl[ibl].antenna1 = i1
+                        wtvis.bl[ibl].antenna2 = i2
+                        wtvis.bl[ibl].time[0] = bvis.data['time'][itime]
+                        wtvis.bl[ibl].freq[0] = 0.0
+                        wtvis.bl[ibl].vis[0] = 0.0
+                        wtvis.bl[ibl].vis[1] = 0.0
+                        for i in range(3):
+                            wtvis.bl[ibl].uvw[i] = bvis.data['uvw'][itime,i2,i1,i]
+                        ibl += 1 
+        # Fill stats
+        status = wtowers.fill_stats_func(wtvis)
         
         # Extracting data from BlockVisibility
         freq = bvis.frequency  # frequency, Hz
         nrows, nants, _, vnchan, vnpol = bvis.vis.shape
         
-        uvw = newbvis.data['uvw'].reshape([nrows * nants * nants, 3])
-        vis = newbvis.data['vis'].reshape([nrows * nants * nants, vnchan, vnpol])
+        #uvw = newbvis.data['uvw'].reshape([nrows * nants * nants, 3])
+        #vis = newbvis.data['vis'].reshape([nrows * nants * nants, vnchan, vnpol])
         
-        vis[...] = 0.0 + 0.0j  # Make all vis data equal to 0 +0j
+        #vis[...] = 0.0 + 0.0j  # Make all vis data equal to 0 +0j
         
         # Get the image properties
         m_nchan, m_npol, ny, nx = model.data.shape
@@ -75,33 +108,41 @@ try:
         #        assert (m_nchan == v_nchan)
         assert (m_npol == vnpol)
         
-        fuvw = uvw.copy()
+        #fuvw = uvw.copy()
         # We need to flip the u and w axes. The flip in w is equivalent to the conjugation of the
         # convolution function grid_visibility to griddata
-        fuvw[:, 0] *= -1.0
-        fuvw[:, 2] *= -1.0
+        #fuvw[:, 0] *= -1.0
+        #fuvw[:, 2] *= -1.0
         
         # Find out the image size/resolution
+        npixdirty = model.nwidth
         pixsize = numpy.abs(numpy.radians(model.wcs.wcs.cdelt[0]))
+        
+        # Define WTowers FoV in direction cosine units
+        theta = numpy.cos(numpy.pi/2 - npixdirty*pixsize)
+        grid_size = nx
         
         # Make de-gridding over a frequency range and pol fields
         vis_to_im = numpy.round(model.wcs.sub([4]).wcs_world2pix(freq, 0)[0]).astype('int')
-        #for vchan in range(vnchan):
-        #    imchan = vis_to_im[vchan]
-        #    for vpol in range(vnpol):
-	        #vis[..., vchan, vpol] = 0.0
-                #vis[..., vchan, vpol] = ng.dirty2ms(fuvw.astype(numpy.float64),
-                #                    numpy.array(freq[vchan:vchan + 1]).astype(numpy.float64),
-                #                    model.data[imchan, vpol, :, :].T.astype(numpy.float64),
-                #                    pixsize_x=pixsize,
-                #                    pixsize_y=pixsize,
-                #                    epsilon=epsilon,
-                #                    do_wstacking=do_wstacking,
-                #                    nthreads=nthreads,
-                #                    verbosity=verbosity)[:,0]
+        for vchan in range(vnchan):
+            imchan = vis_to_im[vchan]
+            for vpol in range(vnpol):
+                # Fill the frequency
+                for ibl in range(wtvis.bl_count):
+                    wtvis.bl[ibl].freq[0] = freq[vchan]
+                wtvis = dirty2vis(model.data[imchan, vpol, :, :].T.astype(numpy.float64), grid_size, theta, wtvis)
+                
+                
+                # Fill the vis and frequency data in wtvis
+                ibl = 0
+                # Loop over ntime and and antennas in BlockVis, copy metadata
+                for itime in range(bvis.data.shape[0]):
+                      for i1 in range(antenna_count):                
+                            for i2 in range(i1+1,antenna_count):
+                                newbvis.data['vis'][itime,i2,i1,vchan,vpol] = wtvis.bl[ibl].vis[0] + 1j*wtvis.bl[ibl].vis[1]
+                                ibl += 1 
         
-        vis = convert_pol_frame(vis, model.polarisation_frame, bvis.polarisation_frame, polaxis=2)
-        newbvis.data['vis'] = vis.reshape([nrows, nants, nants, vnchan, vnpol])
+        newbvis.data['vis'] = convert_pol_frame(newbvis.data['vis'], model.polarisation_frame, bvis.polarisation_frame, polaxis=2)
 
         # Now we can shift the visibility from the image frame to the original visibility frame
         return shift_vis_to_image(newbvis, model, tangent=True, inverse=True)
@@ -131,10 +172,10 @@ try:
         im = copy_image(model)
 
         #ng-related######
-        nthreads = get_parameter(kwargs, "threads", 4)
-        epsilon = get_parameter(kwargs, "epsilon", 1e-12)
-        do_wstacking = get_parameter(kwargs, "do_wstacking", True)
-        verbosity = get_parameter(kwargs, "verbosity", 0)
+        #nthreads = get_parameter(kwargs, "threads", 4)
+        #epsilon = get_parameter(kwargs, "epsilon", 1e-12)
+        #do_wstacking = get_parameter(kwargs, "do_wstacking", True)
+        #verbosity = get_parameter(kwargs, "verbosity", 0)
         ##################
         
         sbvis = copy_visibility(bvis)
@@ -186,9 +227,9 @@ try:
             ms[...] = (1 - flags).astype('complex')
         
         # NG-related
-        if epsilon > 5.0e-6:
-            ms = ms.astype("c8")
-            wgt = wgt.astype("f4")
+        #if epsilon > 5.0e-6:
+        #    ms = ms.astype("c8")
+        #    wgt = wgt.astype("f4")
         ###########################
         
         # Find out the image size/resolution
