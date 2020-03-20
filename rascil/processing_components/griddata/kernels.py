100208c6eb70104cba576b9ad76e59d633351645
timcornwell <realtimcornwell@gmail.com>
2020-03-20 17:40:22 +0000

    Wtowers kernel generation code

--- rascil/processing_components/griddata/kernels.py
+++ rascil/processing_components/griddata/kernels.py
@@ -3,7 +3,8 @@ Functions that define and manipulate kernels
 
 """
 
-__all__ = ['create_pswf_convolutionfunction', 'create_box_convolutionfunction', 'create_awterm_convolutionfunction']
+__all__ = ['create_pswf_convolutionfunction', 'create_box_convolutionfunction', 'create_awterm_convolutionfunction',
+           'convert_kernel_to_list']
 
 import logging
 
@@ -203,6 +204,46 @@ def create_awterm_convolutionfunction(im, make_pb=None, nw=1, wstep=1e15, oversa
         pswf_gcf.data[...] = 1.0
 
     return pswf_gcf, cf
+
+
+def convert_kernel_to_list(gcfcf):
+    """ Convert kernel to form required by w-towers
+
+    :param cf:
+    :return:
+    """
+    gcf, cf = gcfcf
+    nchan, npol, nw, oversampling, _, ny, nx = cf.shape
+    size_y = ny * oversampling
+    size_x = nx * oversampling
+    wplanes = list()
+    for wplane in range(nw):
+        w = cf.grid_wcs.sub([4]).wcs_pix2world(wplane, 0)
+        wslice = numpy.zeros([size_y, size_x], dtype='complex')
+        for y in range(ny):
+            for x in range(ny):
+                for offy in range(oversampling):
+                    for offx in range(oversampling):
+                        wslice[offy + oversampling * y, offx + oversampling * x] = cf.data[
+                            0, 0, wplane, offy, offx, y, x]
+        wplanes.append((w, wslice))
+        if wplane == 0:
+            import matplotlib.pyplot as plt
+            plt.clf()
+            plt.imshow(numpy.real(wslice))
+            plt.show(block=False)
+
+    # int plane_count;
+    # struct w_kernel *kern;
+    # struct w_kernel *kern_by_w;
+    # double w_min, w_max, w_step;
+    # int size_x, size_y;
+    # int oversampling;
+    wmin = cf.grid_wcs.sub([4]).wcs_pix2world(0, 0)
+    wmax = cf.grid_wcs.sub([4]).wcs_pix2world(nw - 1, 0)
+    wstep = cf.grid_wcs.wcs.cdelt[4]
+
+    return (nw, wplanes, wmin, wmax, wstep, oversampling)
 
 
 def convert_image_to_kernel(im: Image, oversampling, kernelwidth):
---
+++ tests/processing_components/test_griddata_wtowers.py
@@ -0,0 +1,52 @@
+""" Unit tests for conversion of kernels to list
+
+
+"""
+import functools
+import logging
+import os
+import unittest
+
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+
+from rascil.data_models.polarisation import PolarisationFrame
+from rascil.processing_components import create_image
+from rascil.processing_components.griddata import convert_kernel_to_list
+from rascil.processing_components.griddata.kernels import create_awterm_convolutionfunction
+from rascil.processing_components.imaging.primary_beams import create_pb_generic
+
+log = logging.getLogger('logger')
+
+log.setLevel(logging.WARNING)
+
+
+class TestGridDataKernels(unittest.TestCase):
+
+    def setUp(self):
+        from rascil.data_models.parameters import rascil_path
+        self.dir = rascil_path('test_results')
+
+        self.phasecentre = SkyCoord(ra=+180.0 * u.deg, dec=-60.0 * u.deg, frame='icrs', equinox='J2000')
+        self.image = create_image(npixel=512, cellsize=0.0005, phasecentre=self.phasecentre,
+                                  polarisation_frame=PolarisationFrame("stokesI"))
+        self.persist = os.getenv("RASCIL_PERSIST", False)
+
+    def test_fill_wterm_to_list(self):
+        gcfcf = create_awterm_convolutionfunction(self.image, make_pb=None, nw=201, wstep=8.0, oversampling=8,
+                                                  support=60, use_aaf=True)
+
+        wtowers = convert_kernel_to_list(gcfcf)
+        print(wtowers)
+
+    def test_fill_awterm_to_list(self):
+        make_pb = functools.partial(create_pb_generic, diameter=35.0, blockage=0.0, use_local=False)
+        pb = make_pb(self.image)
+        gcfcf = create_awterm_convolutionfunction(self.image, make_pb=make_pb, nw=201, wstep=8, oversampling=8,
+                                                  support=60, use_aaf=True)
+        wtowers = convert_kernel_to_list(gcfcf)
+        print(wtowers)
+
+
+if __name__ == '__main__':
+    unittest.main()
