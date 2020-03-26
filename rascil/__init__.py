13ddcbf0cb05679d1d0ed66d861d4c1c6bb26cec
Tim Cornwell <realtimcornwell@gmail.com>
2020-03-26 11:04:50 +0000

    IERS workaround

--- rascil/__init__.py
+++ rascil/__init__.py
@@ -1,12 +1,13 @@
 
+import warnings
 from . import data_models
 from . import processing_components
 from . import workflows
 
 from .processing_components.util.installation_checks import check_data_directory
 
-from astroplan import download_IERS_A
+from astroplan import get_IERS_A_or_workaround
+get_IERS_A_or_workaround()
 
 check_data_directory()
 
-download_IERS_A()
