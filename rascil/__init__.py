a625bfea17c12d676f9e639914ca080e3ee2bc53
Tim Cornwell <realtimcornwell@gmail.com>
2020-03-29 13:51:49 +0100

    Address IERS

--- rascil/__init__.py
+++ rascil/__init__.py
@@ -1,13 +1,15 @@
 
-import warnings
 from . import data_models
 from . import processing_components
 from . import workflows
 
 from .processing_components.util.installation_checks import check_data_directory
 
-from astroplan import get_IERS_A_or_workaround
-get_IERS_A_or_workaround()
+from astropy.utils import iers
 
 check_data_directory()
 
+# This turns off all downloads of the IERS tables. This is a hack until astropy
+# addresses the multiple reader behaviour of the cache reader/updater.
+# TODO: Fix IERS table updates when astropy chanes cache access
+iers.conf.auto_max_age = None
