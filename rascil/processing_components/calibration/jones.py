989a16733c8b0449d10e230344031f122a1e6bb0
Tim Cornwell <realtimcornwell@gmail.com>
2020-03-30 17:44:18 +0100

    Application of vp to images tested

--- rascil/processing_components/calibration/jones.py
+++ rascil/processing_components/calibration/jones.py
@@ -1,16 +1,17 @@
 import numpy
 
 
-def apply_jones(ej, cfs, inverse=False):
+def apply_jones(ej, cfs, inverse=False, min_det=1e-6):
     """ Apply Jones matrix (or inverse)
 
     :param ej: 2x2 Jones matrix
     :param cfs: 2x2 matrix of stokes
     :param inverse: Calculate the inverse
+    :param min_det: Minimum determinant in invert
     :return:
     """
     if inverse:
-        if numpy.abs(numpy.linalg.det(ej)) > 0.0:
+        if numpy.abs(numpy.linalg.det(ej)) > min_det:
             inv_ej = numpy.linalg.inv(ej)
             inv_cej = numpy.conjugate(inv_ej).T
             return inv_ej @ cfs @ inv_cej
