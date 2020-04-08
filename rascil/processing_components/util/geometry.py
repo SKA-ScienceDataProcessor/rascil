c99ed4f57e646032658e29d071a78ef345f7c2c6
Tim Cornwell <realtimcornwell@gmail.com>
2020-04-08 17:40:25 +0100

    Converted astropy geometry calc to CASA
    Revert back once astropy solves cache problem

--- rascil/processing_components/util/geometry.py
+++ rascil/processing_components/util/geometry.py
@@ -1,11 +1,23 @@
 """ Unit tests for geometry
 
+This now uses the CASA measures classes via python-casacore. The interface is not as straightforward
+as astropy.
+
+    https://casa.nrao.edu/docs/CasaRef/measures.measure.html
+
+
 """
 
 __all__ = ['calculate_transit_time', 'calculate_hourangles', 'calculate_azel']
 
+import logging
+
+import numpy
+
 from astropy.time import Time
-from astropy.coordinates import SkyCoord, EarthLocation
+from astropy.coordinates import SkyCoord, EarthLocation, Angle
+
+log = logging.getLogger('logger')
 
 
 def calculate_hourangles(location, utc_time, direction):
@@ -20,13 +32,27 @@ def calculate_hourangles(location, utc_time, direction):
     assert isinstance(location, EarthLocation)
     assert isinstance(utc_time, Time)
     assert isinstance(direction, SkyCoord)
-    
-    from astroplan import Observer
-    site = Observer(location=location)
-    return site.target_hour_angle(utc_time, direction).wrap_at('180d')
+
+    from casacore.measures import measures
+    dm = measures()
+    casa_location = dm.position('itrf', str(location.x), str(location.y), str(location.z))
+    dm.doframe(casa_location)
+    casa_direction = dm.direction('j2000', str(direction.ra), str(direction.dec))
+    has = list()
+    for utc in utc_time:
+        casa_utc_time = dm.epoch('utc', str(utc.mjd) + 'd')
+        dm.doframe(casa_utc_time)
+        casa_hadec = dm.measure(casa_direction, 'hadec')
+        ha = str(casa_hadec['m0']['value']) + casa_hadec['m0']['unit']
+        has.append(Angle(ha))
+    return has
+
+    # from astroplan import Observer
+    # site = Observer(location=location)
+    # return site.target_hour_angle(utc_time, direction).wrap_at('180d')
 
 
-def calculate_transit_time(location, utc_time, direction):
+def calculate_transit_time(location, utc_time, direction, fraction_day=0.01):
     """ Find the UTC time of the nearest transit
 
     :param utc_time:
@@ -38,9 +64,21 @@ def calculate_transit_time(location, utc_time, direction):
     assert isinstance(utc_time, Time)
     assert isinstance(direction, SkyCoord)
 
-    from astroplan import Observer
-    site = Observer(location)
-    return site.target_meridian_transit_time(utc_time, direction, which="next", n_grid_points=100)
+    utc_times = Time(numpy.arange(0.0, 1.0, fraction_day) + utc_time.mjd, format='mjd', scale='utc')
+    _, els = calculate_azel(location, utc_times, direction)
+    elmax = - numpy.pi / 2.0
+    best = None
+    for i, el in enumerate(els):
+        if el.value > elmax:
+            elmax = el.value
+            best = i
+    if elmax < 0.0:
+        log.warning("Source is always below horizon")
+    return utc_times[best]
+    
+    # from astroplan import Observer
+    # site = Observer(location)
+    # return site.target_meridian_transit_time(utc_time, direction, which="next", n_grid_points=100)
 
 
 def calculate_azel(location, utc_time, direction):
@@ -55,7 +93,25 @@ def calculate_azel(location, utc_time, direction):
     assert isinstance(utc_time, Time)
     assert isinstance(direction, SkyCoord)
 
-    from astroplan import Observer
-    site = Observer(location=location)
-    altaz = site.altaz(utc_time, direction)
-    return altaz.az.wrap_at('180d'), altaz.alt
+    # Use the casa measures
+    from casacore.measures import measures
+    dm = measures()
+    casa_location = dm.position('itrf', str(location.x), str(location.y), str(location.z))
+    dm.doframe(casa_location)
+    casa_direction = dm.direction('j2000', str(direction.ra), str(direction.dec))
+    azs = list()
+    els = list()
+    for utc in utc_time:
+        casa_utc_time = dm.epoch('utc', str(utc.mjd) + 'd')
+        dm.doframe(casa_utc_time)
+        casa_azel = dm.measure(casa_direction, 'azel')
+        az = str(casa_azel['m0']['value']) + casa_azel['m0']['unit']
+        el = str(casa_azel['m1']['value']) + casa_azel['m1']['unit']
+        azs.append(Angle(az))
+        els.append(Angle(el))
+    return azs, els
+
+    # from astroplan import Observer
+    # site = Observer(location=location)
+    # altaz = site.altaz(utc_time, direction)
+    # return altaz.az.wrap_at('180d'), altaz.alt
