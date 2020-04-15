86dc0d1ae974c2f84d47f844bfa0508a514225c6
Tim Cornwell <realtimcornwell@gmail.com>
2020-04-15 17:38:11 +0100

    Skip large imaging cases

--- tests/processing_components/test_imaging.py
+++ tests/processing_components/test_imaging.py
@@ -193,6 +193,7 @@ class TestImaging2D(unittest.TestCase):
                                                   oversampling=4, support=100, use_aaf=True)
         self._predict_base(fluxthreshold=35.0, name='predict_awterm', gcfcf=gcfcf)
 
+    @unittest.skip("Tested in rsexecute")
     def test_predict_awterm_spec(self):
         self.actualSetUp(zerow=False, freqwin=5)
         make_pb = functools.partial(create_pb_generic, diameter=35.0, blockage=0.0, use_local=False)
@@ -200,6 +201,7 @@ class TestImaging2D(unittest.TestCase):
                                                   oversampling=4, support=100, use_aaf=True)
         self._predict_base(fluxthreshold=35.0, name='predict_awterm_spec', gcfcf=gcfcf)
 
+    @unittest.skip("Tested in rsexecute")
     def test_predict_awterm_spec_IQUV(self):
         self.actualSetUp(zerow=False, freqwin=5, image_pol=PolarisationFrame("stokesIQUV"))
         make_pb = functools.partial(create_pb_generic, diameter=35.0, blockage=0.0, use_local=False)
@@ -214,6 +216,7 @@ class TestImaging2D(unittest.TestCase):
                                                   oversampling=4, support=100, use_aaf=True)
         self._invert_base(name='invert_awterm', positionthreshold=35.0, check_components=False, gcfcf=gcfcf)
 
+    @unittest.skip("Tested in rsexecute")
     def test_invert_awterm_spec(self):
         self.actualSetUp(zerow=False, freqwin=5)
         make_pb = functools.partial(create_pb_generic, diameter=35.0, blockage=0.0, use_local=False)
@@ -221,6 +224,7 @@ class TestImaging2D(unittest.TestCase):
                                                   oversampling=4, support=100, use_aaf=True)
         self._invert_base(name='invert_awterm_spec', positionthreshold=35.0, check_components=False, gcfcf=gcfcf)
 
+    @unittest.skip("Tested in rsexecute")
     def test_invert_awterm_spec_IQUV(self):
         self.actualSetUp(zerow=False, freqwin=5, image_pol=PolarisationFrame("stokesIQUV"))
         make_pb = functools.partial(create_pb_generic, diameter=35.0, blockage=0.0, use_local=False)
